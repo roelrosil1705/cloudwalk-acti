480b1840c574a0060a39caa6ca0a737cbb9d07fa
Henryblinds <hbg_009@ymail.com>
2016-06-20 18:39:53 +0800

    Design Pages
    Design Pages

--- application/views/newspage_view.php
+++ application/views/newspage_view.php
@@ -19,7 +19,7 @@
     <script src="<?php echo base_url();?>elastslide/js/modernizr.custom.17475.js"></script><!-- added 01-03-2014 -->
 
 </head>
-<body style="background-image:url('img/bg-workwithus.png');">
+<body style="background:url('<?php echo base_url() ?>img/bg-news.png') 50% 0 no-repeat fixed">
 <div id="wrapper" style="border:solid px green;">
     <div class="">
         <div class="well well-md well-sm" style="background:transparent;border:none;">
@@ -91,86 +91,60 @@
     </div>
     <div class="curtains" style="margin-top: -36px;">
         <div>
-            <div id="news-aai" style="min-height: 100%;" class="aai-background news-para section-aai">
+            <div style="min-height: 100%;" class="aai-background news-para">
                 <div id="news-sec" class="container" style="border:solid px red;margin-top:0px;">
 
                     <div class="row" style="border:solid px red;margin-top:150px;">
-                        <div class="col-md-4 col-md-offset-2 col-sm-4">
-                            <!--<p class="news_title">news</p>-->
-                            <img src="<?php echo base_url(); ?>images/news-title.png">
+                        <div class="col-md-12 col-sm-4">
+                            <img src="<?php echo base_url(); ?>images/news-title.png" style="max-width: 431px;display: block;margin: 0 auto;">
                         </div>
                     </div>
-                    <!--
-                        <div class="row aai-news-bshadow" style="">
-                            <div class="container pull-left" style="margin-top: 8px;">
-
-                                <div class="col-md-1">
-                                    <a href="#"><img src="img/twitter.png"></a>
-                                </div>
-
-                                <div class="col-md-12">
-                                    <p><span><b>Activation Advertising</b></span> uploaded a new photo in the album <b>Rexona Run</b></p>
-                                </div>
+                    <!--Start PAGE -->
+                    <div class="row col-md-10 col-md-offset-1 aai-news-bshadow" style="min-height: 800px;">
+                        <div class="col-md-12" style="border-bottom:solid px gray;padding: 70px 15px;text-align: justify;">
+                            <div class="container" style="font-weight: 100;font-family: sans-serif;padding-top: 25%;font-size: 1.2em;">
+                                <!--Start CONTENT -->
+                                <p>As of June 20, 2016, we at Activations Advertising, Inc. continue to cooperate with the NBI and the Congress regarding the investigation on Closeup Forever Summer 2016 . The deaths that occurred during the event are a terrible tragedy and have been profoundly traumatic for all of the parties involved.</p>
+                                <p>In the interest of full disclosure, we would like to make public the documents, which we have submitted, to the NBI –which includes the security plan validated by affidavits. We have decided to publish this so that the public can be informed about the details of this unfortunate circumstance.</p>
+                                <p>Currently, our organization is working with several security experts to find possible enhancements to our security protocol.</p>
+                                <!--End CONTENT -->
                             </div>
                         </div>
-                    -->
-                    <!--
-                        <div class="row aai-news-bshadow">
-                            <div class="container pull-left" style="margin-top: 8px;">
-
-                                <div class="col-md-1">
-                                    <a href="https://www.facebook.com/activationsadvertisinginc" target="_blank"><img src="img/fb.png"></a>
-                                </div>
-
-                                <div class="col-md-12">
-                                    <p><span><b>Activation Advertising</b></span> uploaded a new photo in the album <b>Rexona Run</b></p>
-                                </div>
-                            </div>
-                        </div>
-                    -->
-                    <div class="row aai-news-bshadow">
-                        <div class="col-md-12" style="border-bottom:solid px gray;padding: 15px;">
-                            <div class="container">
+                    </div>
+                    <!--End PAGE -->
+                    <!--Start PAGE -->
+                    <div class="row col-md-10 col-md-offset-1 aai-news-bshadow" style="min-height: 800px;">
+                        <div class="col-md-12" style="border-bottom:solid px gray;padding: 70px 15px;text-align: justify;">
+                            <div class="container" style="font-weight: 100;font-family: sans-serif;">
+                                <!--Start CONTENT -->
                                 <p><span><strong>Activations brings excitement at every turn with the launch of the Samsung Gear S2 and Samsung Galaxy Studio.</strong></span></p>
                                 <p>Hundreds of media and influencers from the tech scene flocked to the SM Mall of Asia where, for the first time ever, a big event was held at the iconic Globe Rotunda in front of the Mall.
                                     Activations Advertising played a leading role in creating one of the biggest tech launches ever in the country with the arrival of Samsung's latest smartwatch, the Gear S2. We converted the MOA globe into the biggest smartwatch ever in a live interactive display to celebrate the product's launch.
                                     The night also saw the opening of the Samsung Galaxy Studio at the SM MOA Cyberzone as it makes its exclusive stop here in the Philippines on its worldwide tour. The booth activation featured exciting experience zones headline by Samsung's latest line of smartphones with the Gear S2 taking center stage.
                                     Check out the Samsung Galaxy Studio at the SM Mall of Asia Cyberzone from November 27 to December 5, 2015.
                                     #GalaxyStudioPH #GearsS2</p>
-                            </div>
-                            <div class="container">
-                                <p><span><strong>Powering-up 1 MILLION Future-ready teen AGAIN!!!</strong></span></p>
-                                <p>#TeenWeekPH 2015 has just wrapped-up it's nationwide celebrations of all things teens. We visited more than 200 high-schools throughout the Philippines where we enjoyed, celebrated and shared wonderful experiences with more than 1 million students.
-                                    The 1-week celebrations aimed to educate Filipino teens about the important topics that are essential to a their personal development like personal care, hygiene, self-esteem. ambitions and future careers, which were made more fun and exciting with the help of Creamsilk, Rexona and Pond's.
-                                    From enlightening TED-talk forums, fun games, and shareable photo moments, students took-in important learnings while still having a good time all week long.
-                                    The celebrations concluded with the first-ever TeenWeek Fair headlined by a talent showcase featuring outstanding live performances from the students themselves. Our favorite Unilever brand ambassadors and celebrities like were also there to share the celebrations with our students.
-                                    In it's 3rd year of running, #TeenWeekPH has added 1 million more young teens who are future-ready to take on the world.
-                                    See you all next year!</p>
-                            </div>
-                            <div class="container" style="border-bottom:solid px gray; margin-top: 10px;">
-                                <p><span><strong>Activations Advertising Inc. breaks another world record.</strong></span></p>
-                                <p>AAI has bagged another world record after it set the official Guinness World Record for the most number of people handwashing a single piece of fabric
-                                    simultaneously for Breeze Detergent.
-                                    Fighting through torrential rain and the massive logistics and planning nightmare of an official world record attempt, the agency successfully
-                                    gathered more than 2,200 moms at the UMAK Track & Field who were able to successfully handwash a 1 Kilometer long piece of cloth all at the same time.
-                                    The record-breaking attempt was the perfect showcase of Breeze's superiority over other detergent brands and it's a quality is now etched in the record books.
-                                    This is also the agency's second successful record attempt after its Mentos Geyser record which was set in 2010 <!--<a href="#" class="aai-news-link"><strong>Read More...</strong></a>--></p>
-                            </div>
-                            <!--<hr class="featurette-divider">-->
-                            <!--<div>&nbsp;</div>-->
-                        </div>
-                        <!--<div>&nbsp;</div>-->
-                        <!--
-                        <div class="col-md-12" style="border-bottom:solid px gray;">
-                            <div class="container" style="margin-top: 10px;">
-                                <p><span><b>Activation Advertising Wins Special Award for Ambient Media Advertising!</b></span></p>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam at scelerisque ipsum. Sed at tellus ut lectus
-                                aliquam fermentum ut nec mauris. Nullam quis dolor ornare, lobortis orci et, ullamcorper metus. Aliquam dignissim
-                                sed urna nec suscipit. Donec tincidunt, felis pharetra gravida commodo, nulla quam mattis sem, et dignissim augue enim a lacus. <a href="#" class="aai-news-link"><b>Read More...</b></a></p>
+                                <!--End CONTENT -->
                             </div>
                         </div>
-                        -->
                     </div>
+                    <!--End PAGE -->
+                    <!--Start PAGE -->
+                    <div class="row col-md-10 col-md-offset-1 aai-news-bshadow" style="min-height: 800px;margin-bottom: 60px;">
+                        <div class="col-md-12" style="border-bottom:solid px gray;padding: 70px 15px;text-align: justify;">
+                            <div class="container" style="font-weight: 100;font-family: sans-serif;">
+                                <!--Start CONTENT -->
+                                <p><span><strong>Activations brings excitement at every turn with the launch of the Samsung Gear S2 and Samsung Galaxy Studio.</strong></span></p>
+                                <p>Hundreds of media and influencers from the tech scene flocked to the SM Mall of Asia where, for the first time ever, a big event was held at the iconic Globe Rotunda in front of the Mall.
+                                    Activations Advertising played a leading role in creating one of the biggest tech launches ever in the country with the arrival of Samsung's latest smartwatch, the Gear S2. We converted the MOA globe into the biggest smartwatch ever in a live interactive display to celebrate the product's launch.
+                                    The night also saw the opening of the Samsung Galaxy Studio at the SM MOA Cyberzone as it makes its exclusive stop here in the Philippines on its worldwide tour. The booth activation featured exciting experience zones headline by Samsung's latest line of smartphones with the Gear S2 taking center stage.
+                                    Check out the Samsung Galaxy Studio at the SM Mall of Asia Cyberzone from November 27 to December 5, 2015.
+                                    #GalaxyStudioPH #GearsS2</p>
+                                <!--End CONTENT -->
+                            </div>
+                        </div>
+                    </div>
+                    <!--End PAGE -->
+
                 </div>
             </div>
         </div>
